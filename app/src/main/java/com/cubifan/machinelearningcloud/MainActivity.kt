e36abe322de8e542a5baf3323419e18cd65e5c6a
Lucas Maillard <lucas.maillard8@gmail.com>
2022-02-11 17:08:40 +0100

    colors and home page

--- app/src/main/java/com/cubifan/machinelearningcloud/MainActivity.kt
+++ app/src/main/java/com/cubifan/machinelearningcloud/MainActivity.kt
@@ -3,37 +3,87 @@ package com.cubifan.machinelearningcloud
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.cubifan.machinelearningcloud.ui.theme.MachineLearningCloudTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             MachineLearningCloudTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    homePage()
                 }
             }
         }
     }
 }
 
+@Preview(name = "Home page")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun homePage() {
+    Image(
+        painter = painterResource(id = R.drawable.cloudsbackground), null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxSize()
+    )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(.2f)) {
+            Text(
+                text = "What is this cloud ?",
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+            )
+        }
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(.2f)
+                .align(Alignment.BottomStart)
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .size(150.dp, 70.dp)
+            ) {
+                Text("See a cloud", fontSize = 16.sp)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MachineLearningCloudTheme {
-        Greeting("Android")
+        Image(painter = painterResource(R.drawable.cloudsbackground), null)
+        Text("What is this cloud?")
     }
 }
--- app/src/main/java/com/cubifan/machinelearningcloud/ui/theme/Color.kt
+++ app/src/main/java/com/cubifan/machinelearningcloud/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.cubifan.machinelearningcloud.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val paleturquoise = Color(0xFFAFEEEE)
+val turquoise = Color(0xFF40E0D0)
+val cadetBlue = Color(0xFF5F9EA0)
